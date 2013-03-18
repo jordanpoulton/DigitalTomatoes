65eae34c2fc8bcb179eee5184ba7e072e618d276
jordanpoulton <jordan@oxfordentrepreneurs.co.uk>
2013-03-18 10:10:54 +0000

    First commit of the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DigitalTomatoes::Application.config.secret_token = '83da1534f5de9e58d5d485fad43681400519585b537391601adcef98787fe163d0262570452d9810d16f070dc98f82b28f5c8fe2b805e3585e1937355274d279'
